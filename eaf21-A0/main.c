df38b183cec67eb9e025483f42e8b6142577ba07
eduardaafreitas <eduardaaguiar1012@hotmail.com>
2023-08-09 12:20:25 -0300

    segfault

--- eaf21-A0/main.c
+++ eaf21-A0/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /*--------------------------structs--------------------------*/
 
@@ -20,14 +21,20 @@ typedef struct fila_t {
 
 void requisita(char *titulo, char *texto){
     getchar();
-    printf("\nDigite o título: ");
+    printf("\t Digite o título: ");
     fgets(titulo, 33, stdin);
-    printf("Digite o texto: ");
+    printf("\t Digite o texto: ");
     fgets(texto, 513, stdin);
 }
 
+int fila_vazia(fila_t* fila){
+    if (fila->comeco == NULL)
+        return 1;
+    return 0;
+}
+
 fila_t* cria_fila() {
-    fila_t* fila = malloc(sizeof(fila));
+    fila_t* fila = malloc(sizeof(fila_t));
     if (fila == NULL) {
         printf("\n Erro ao alocar memória para a fila");
         exit(0);
@@ -42,34 +49,35 @@ void insere_fim(fila_t* fila, noticia_t* nova_noticia) {
 	if (fila_vazia(fila) == 1){
 		fila->comeco = nova_noticia;
 		fila->fim = nova_noticia;
+        printf("\t if insere fim ok \n");
 	} else {
 		//adiciona o novo e atualiza o ponteiro para o fim
 		nova_noticia->anterior = fila->fim;
 		fila->fim->prox = nova_noticia;
 		fila->fim = nova_noticia;
+        printf("\t else insere else fim ok \n");
 	}
+    printf("\t insere fim foiiiii \n");
 }
 
 void retira_comeco(fila_t* fila){
 	noticia_t* aux = fila->comeco;
-
+    printf("\t retira começo antes if \n");
 	if (aux->prox != NULL){
 		fila->comeco = aux->prox;
 		aux->prox->anterior = NULL;
-		aux->prox = NULL;
+		//aux->prox = NULL;
+        printf("\t if retira começo ok \n");
 	} else {
 		fila->comeco = NULL;
 		fila->fim = NULL;
+        printf("\t else retira começo ok \n");
 	}
-
+    printf("\t retira começo antes free \n");
 	free(aux);
+    printf("\t retira começo depois free \n");
 }
 
-int fila_vazia(fila_t* fila){
-    if (fila->comeco = NULL)
-        return 1;
-    return 0;
-}
 
 void destroi_fila(fila_t* fila){
     while (fila_vazia(fila)) {
@@ -80,54 +88,43 @@ void destroi_fila(fila_t* fila){
 
 void cadastro( fila_t* breaknews, fila_t* informes){
     int tipo_noticia;
-    noticia_t* nova_noticia;
-    printf("\t Digite o tipo da noticia: ");
-    scanf("%d", &tipo_noticia);
+    noticia_t* nova_noticia = malloc(sizeof(noticia_t));
+
     nova_noticia->titulo = malloc(sizeof(char) * 33);
     nova_noticia->texto = malloc(sizeof(char) * 513);
     nova_noticia->idade = 0;
-    requisita(noticia_topo->titulo, noticia_topo->texto);
+
+    requisita(nova_noticia->titulo, nova_noticia->texto);
+
+    printf("\t Digite o tipo da noticia: ");
+    scanf("%d", &tipo_noticia);
 
     if (tipo_noticia == 0){
-        insere_fim(breaknews, noticia_topo);
+        insere_fim(breaknews, nova_noticia);
     } else {
-        insere_fim(informes, noticia_topo);
+        insere_fim(informes, nova_noticia);
     }
-    
-}
-
-void finaliza_edicao( fila_t* breaknews, fila_t* informes){
-    fila_t* jornal = cria_fila();
-    verifica_jornal(breaknews, informes, jornal);
-    imprime_jornal(jornal);
-    destroi_fila(breaknews);
-    destroi_fila(informes);
-    destroi_fila(jornal);
+    return;
 }
 
 void retira_invalidas(fila_t* fila){
+    noticia_t* aux = fila->comeco;
     if (fila_vazia(fila) == 1){
         return;
-    }
-    else{    
-        while (fila->comeco != NULL){
-            if (fila->comeco->idade > 3){
-                retira_comeco(fila);
-            }
+    }   
+    while (aux != NULL){
+        if (fila->comeco->idade > 3){
+            retira_comeco(fila);
         }
+        aux = aux->prox;
     }
 }
 
+void verifica_jornal(fila_t* breaknews, fila_t* informes, fila_t* jornal){
+    noticia_t* aux_breaknews = breaknews->comeco;
+    noticia_t* aux_informes = informes->comeco;
 
-int verifica_jornal(fila_t* breaknews, fila_t* informes, fila_t* jornal){
-    if (fila_vazia(breaknews) == 1 && fila_vazia(informes) == 1){
-        printf("\t Esta edição foi pulada por falta de notícias!\n");
-        return 0;
-    }
-    retira_invalidas(breaknews); 
-    retira_invalidas(informes);
-
-    while (fila_vazia(breaknews) == 0 && fila_vazia(informes) == 0){
+    while ((aux_breaknews != NULL) && (aux_informes != NULL) ){
         if (breaknews->comeco->idade < informes->comeco->idade){
             insere_fim(jornal, breaknews->comeco);
             retira_comeco(breaknews);
@@ -135,29 +132,9 @@ int verifica_jornal(fila_t* breaknews, fila_t* informes, fila_t* jornal){
             insere_fim(jornal, informes->comeco);
             retira_comeco(informes);
         }
+        aux_breaknews = aux_breaknews->prox;
+        aux_informes = aux_informes->prox;
     }
-}
-
-int menu(){
-    int opcao;
-    imprime_header();
-    printf("\t Digite a opcao desejada: ");
-    scanf("%d", &opcao);
-    printf("\n");
-    while (opcao < 1 || opcao > 3){
-        printf("\t Opcao invalida, digite novamente: ");
-        scanf("%d", &opcao);
-    }
-    return opcao;
-}
-
-void imprime_header(){
-
-    printf("\t ----------- Menu Principal ----------- \n");
-    printf("\t 1 - Cadastrar noticia\n");
-    printf("\t 2 - Fechar edicao\n");
-    printf("\t 3 - Sair do programa\n");
-    printf("\t -------------------------------------- \n");
 }
 
 void imprime_jornal( fila_t* jornal){
@@ -171,6 +148,45 @@ void imprime_jornal( fila_t* jornal){
     }
 }
 
+void finaliza_edicao( fila_t* breaknews, fila_t* informes){
+    fila_t* jornal = cria_fila();
+    if (fila_vazia(breaknews) == 1 && fila_vazia(informes) == 1){
+        printf("\t Não há notícias para serem impressas!\n");
+        return;
+    }
+    retira_invalidas(breaknews);
+    retira_invalidas(informes);
+    verifica_jornal(breaknews, informes, jornal);
+    imprime_jornal(jornal);
+    destroi_fila(breaknews);
+    destroi_fila(informes);
+    destroi_fila(jornal);
+}
+
+void imprime_header(){
+
+    printf("\t ----------- Menu Principal ----------- \n");
+    printf("\t 1 - Cadastrar noticia\n");
+    printf("\t 2 - Fechar edicao\n");
+    printf("\t 3 - Sair do programa\n");
+    printf("\t -------------------------------------- \n");
+}
+
+
+int menu_inicial(){
+    int opcao;
+    imprime_header();
+    printf("\t Digite a opcao desejada: ");
+    scanf("%d", &opcao);
+    printf("\n");
+    while (opcao < 1 || opcao > 3){
+        printf("\t Opcao invalida, digite novamente: ");
+        scanf("%d", &opcao);
+    }
+    return opcao;
+}
+
+
 /*-----------------------------------------------------------*/
 
 
@@ -183,20 +199,21 @@ int main(){
     fila_t* breaknews = cria_fila();
     fila_t* informes = cria_fila();
     int opcao;
-    opcao = menu();
-    while(opcao != 0){
+    opcao = menu_inicial();
+    while(opcao != 3){
         switch (opcao){
         case 1:
             cadastro(breaknews, informes);
             break;
         
         case 2:
-            finaliza_edicao(breaknews, informes );
+            finaliza_edicao(breaknews, informes);
             break;
         
         case 3:
             exit(0);
         }
+        opcao = menu_inicial();
     }
     return 0;
 }
