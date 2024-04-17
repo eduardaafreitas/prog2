a897b7177edde4a133b31064ec0202de50f90811
eduardaafreitas <eduardaaguiar1012@hotmail.com>
2024-04-17 14:49:49 -0300

    ok

---
+++ a2/eaf21-A2/csvreader.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "io.h"
+#include "lib.h"
+
+
+int main( int argc, char *argv[]){
+    int opt = 0;  
+
+    if (argc < 1){
+        fprintf(stderr, "Forma de uso: ./csvreader <arq_in> \n");
+        exit(1);
+    }
+    
+    FILE *archive = fopen(argv[1], "r");
+    if (!archive){
+        fprintf(stderr, "Erro ao abrir o archive!\n");
+        exit(2);
+    }   
+    
+    unsigned long column, row;
+    column = count_columns(archive);
+    fseek(archive, 0, SEEK_SET);
+
+    row = count_rows(archive);
+    fseek(archive, 0, SEEK_SET);
+
+    //aloca memorias para as structs:
+    csv *keeper = alloc_csv();
+    base *database = alloc_database();
+    base *database_copy = alloc_database();
+       
+    //armazena o arquivo csv na struct base
+    layin_csv(archive, keeper, database, row, column);
+    fseek(archive, 0, SEEK_SET);
+    // count_stringsize(archive, keeper, row, column);
+    
+//--------------------------------------------------------
+
+    printf("==== LEITOR DE ARQUIVOS CSV ====\n");
+    options();
+
+    scanf("%d", &opt); //selecionando opcao desejada
+
+    while (opt != 9) {
+        if (opt == 1){
+            sumario(keeper, database);
+        }
+        else if (opt == 2){
+            mostrar(keeper, database);
+        }
+        else if (opt == 3){
+            layin_csv(archive, keeper, database_copy, row, column);
+            filtrar(database, keeper, database_copy);
+        }
+        else if (opt == 4){
+            printf("descricao em desenvolvimento!\n");
+        }
+        else if (opt == 5){
+            printf("ordenacao em desenvolvimento!\n");
+        }
+        else if (opt == 6){
+            printf("selecao em desenvolvimento!\n");
+        }
+        else if (opt == 7){
+            printf("dados faltantes em desenvolvimento!\n");
+        }
+        else if (opt == 8){
+            printf("salvamento de dados em desenvolvimento!\n");
+        }
+        else{
+            fprintf(stderr, "Digite uma opcao valida!\n");
+        }
+        printf("Pressione Enter para continuar\n\n");
+        getchar();
+        options();
+        opt = 0;
+        scanf("%d", &opt);
+    }
+
+    free_csv(keeper);
+    free_database(database);
+    printf("Fim do programa!\n");
+    return 0 ;
+}
---
+++ a2/eaf21-A2/io.h
@@ -0,0 +1,50 @@
+//desenvolvido por Eduarda de Aguiar Freitas, GRR 20211799
+#ifndef __IO__
+#define __IO__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+typedef struct csv csv;
+struct csv {
+    FILE *archive;
+    unsigned long row;
+    unsigned long column;
+    char *type;
+    long pos;
+    size_t *sizes;
+};
+
+typedef struct base base;
+struct base {
+    unsigned long row;
+    unsigned long column;
+    char ***data;
+};
+
+
+unsigned long count_columns(FILE *archive);
+
+unsigned long count_rows(FILE *archive);
+
+csv* alloc_csv();
+
+base *alloc_database();
+
+void count_stringsize(FILE *archive, csv *keeper, unsigned long row, unsigned long column);
+
+void layin_csv(FILE *archive, csv *keeper, base *database, unsigned long row, unsigned long column);
+
+void sumario(csv *keeper, base *database);
+
+void mostrar(csv *keeper, base *database);
+
+void filtrar(base *database, csv *keeper, base *database_copy);
+
+void free_csv(csv *keeper_csv);
+void free_database(base *database);
+
+
+#endif
